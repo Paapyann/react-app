dfa66e22338e4a0cde4d445f5ca851cd6289e3c4
Levon <lpapyan10@gmail.com>
2023-11-13 19:29:21 +0400

    lastproject2

--- src/Components/AboutUS/About.jsx
+++ src/Components/AboutUS/About.jsx
@@ -5,6 +5,7 @@ import Row from "react-bootstrap/Row";
 import img1 from "../../images/handsome_pic_vtc.jpg";
 import img2 from "../../images/IMG_6468.jpg";
 import logos from "../../images/Untitled.png";
+import Styles from "./styles.module.css";
 
 const About = () => {
   return (
@@ -24,28 +25,17 @@ const About = () => {
     //   </div>
     // </div>
     <div>
-      <h3 style={{ textAlign: "center" }}>Team members</h3>
-      <div
-        style={{
-          backgroundColor: "beige",
-          width: "70%",
-          height: "500px",
-          display: "flex",
-          justifyContent: "space-around",
-          margin: "auto",
-        }}
-      >
+      <h3>Team members</h3>
+      <div className={Styles.maindiv}>
         <div>
-          <Image src={img1} rounded style={{ marginTop: "20%" }} />
-          <p style={{ fontSize: "20px", textAlign: "center" }}>Levon Papyan</p>
-          <p style={{ fontSize: "16px", textAlign: "center" }}>
-            Page developer
-          </p>
+          <Image src={img1} rounded style={{ marginTop: "40%" }} />
+          <p className={Styles.Membername}>Levon Papyan</p>
+          <p className={Styles.description}>Page developer</p>
         </div>
         <div>
-          <Image src={img2} rounded style={{ marginTop: "20%" }} />
-          <p style={{ fontSize: "20px", textAlign: "center" }}>Nane Dnoyan</p>
-          <p style={{ fontSize: "16px", textAlign: "center" }}>Page designer</p>
+          <Image src={img2} rounded style={{ marginTop: "40%" }} />
+          <p className={Styles.Membername}>Nane Dnoyan</p>
+          <p className={Styles.description}>Page designer</p>
         </div>
       </div>
     </div>
--- src/Components/ContactUS/contact.jsx
+++ src/Components/ContactUS/contact.jsx
@@ -1,25 +1,41 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Styles from "./styles.module.css";
 
 function TextControlsExample() {
   return (
-    <div style={{ margin: "auto" }}>
-      <Form>
-        <h3 style={{ textAlign: "center" }}>Any questions?</h3>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            style={{ width: "50%" }}
-            type="email"
-            placeholder="name@example.com"
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Describe your problem</Form.Label>
-          <Form.Control style={{ width: "50%" }} as="textarea" rows={3} />
-        </Form.Group>
-        <Button variant="success">Send</Button>{" "}
-      </Form>
+    <div>
+      <h3 style={{ textAlign: "center" }}>Any questions?</h3>
+      <br />
+      <br />
+      <br />
+      <div className={Styles.maindiv}>
+        <Form>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label className={Styles.labels} style={{ marginTop: "1%" }}>
+              Email address
+            </Form.Label>
+            <Form.Control
+              className={Styles.formcontrol}
+              type="email"
+              placeholder="name@example.com"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label className={Styles.labels}>
+              Describe your problem
+            </Form.Label>
+            <Form.Control
+              className={Styles.formcontrol}
+              as="textarea"
+              rows={3}
+            />
+          </Form.Group>
+          <Button variant="success" className={Styles.button}>
+            Send
+          </Button>{" "}
+        </Form>
+      </div>
     </div>
   );
 }
